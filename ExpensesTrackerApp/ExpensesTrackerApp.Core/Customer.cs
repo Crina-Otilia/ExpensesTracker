244987f0b385dfb1d9ee025d5df9563c86ceba2b
Crina-Otilia <purice.crina@yahoo.com>
2020-05-26 21:25:34 +0300

    Create the Core and Classes

---
+++ ExpensesTrackerApp/ExpensesTrackerApp.Core/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpensesTrackerApp.Core
+{
+    class Customer:IEntityBase
+    {
+        public Customer()
+        {
+            Income = new List<Income>();
+            Expenses = new List<Expenses>();
+        }
+
+        public Guid Id { get; set; }
+
+        public string IdNo { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FullName { get; set; }
+
+        public virtual IList<Income> Income { get; set; }
+
+        public virtual IList<Expenses> Expenses { get; set; }
+    }
+}
---
+++ ExpensesTrackerApp/ExpensesTrackerApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExpensesTrackerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
